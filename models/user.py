10b3eb9fcdfdeab93a89e031a11e999b6ba6df24
gregcdjm <gregoirecoudrin@gmail.com>
2022-11-24 18:02:51 +0100

    task7

--- models/user.py
+++ models/user.py
@@ -5,6 +5,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 from models.base_model import Base
 
+base = declarative_base()
+
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
