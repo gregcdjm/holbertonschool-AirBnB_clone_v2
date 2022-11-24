fe6c990ae9ad123b5f9e1856a17080b41cac4cc9
gregcdjm <gregoirecoudrin@gmail.com>
2022-11-24 18:08:11 +0100

    task7

--- models/user.py
+++ models/user.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
-from models.base_model import Base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
-Base = declarative_base()
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = 'users'
-    email = Column(String[128], nullable=False)
-    password = Column(String, nullable=False)
-    first_name = Column(String, nullable=False)
-    last_name = Column(String, nullable=False)
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
