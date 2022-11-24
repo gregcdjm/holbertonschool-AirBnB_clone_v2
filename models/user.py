a3a3dbcf5285f37f2bf3cdf58a57e34d60667473
gregcdjm <gregoirecoudrin@gmail.com>
2022-11-24 17:55:51 +0100

    task7

--- models/user.py
+++ models/user.py
@@ -2,7 +2,7 @@
 """This module defines a class User"""
 from models.base_model import BaseModel
 
-class User(BaseModel.Base):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
     email = Column(String[128], nullable=False)
