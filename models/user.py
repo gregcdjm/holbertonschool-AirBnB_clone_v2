453593c08fa3e33fb543555d0855e227ffb5a380
gregcdjm <gregoirecoudrin@gmail.com>
2022-11-24 17:49:26 +0100

    yask7

--- models/user.py
+++ models/user.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
-hbnb_dev_db.users= [["user"]]
+from sqlalchemy.ext.declarative import declarative_base
+
+BaseModel = declarative_base()
+
 class User(BaseModel.Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
     email = Column(String[128], nullable=False)
     password = Column(String, nullable=False)
     first_name = Column(String, nullable=False)
     last_name = Column(String, nullable=False)
-
-    User.addresses = relationship(
-    "Address", order_by=Address.id, back_populates="user")
