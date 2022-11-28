30184cad565d1ab82a2061e79f24d16d52179ba8
gregcdjm <gregoirecoudrin@gmail.com>
2022-11-28 13:48:35 +0100

    docu

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3 __init__.py
-
+"""fffffkfkkfkfk"""
 import Flask from flask
-
+"""fffkfkfkfk"""
 app = Flask(__name__)
-
+"""fjfjfjjf"""
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
