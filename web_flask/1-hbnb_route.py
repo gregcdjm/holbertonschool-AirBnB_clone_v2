54d01bb8fbd1795ab1e6c63efc40b61a6012ab12
gregcdjm <gregoirecoudrin@gmail.com>
2022-11-28 14:30:09 +0100

    1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""flask app"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_flask():
+    """Retunr hello"""
+    return 'Hello HBNB!'
+@app.route('/hbnb')
+def hello_flask():
+    """Retunr hello"""
+    return 'HBNB'
+
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
