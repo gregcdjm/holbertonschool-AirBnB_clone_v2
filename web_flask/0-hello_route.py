152f13342b50aa321190b7402f70fb43c40a8c02
gregcdjm <gregoirecoudrin@gmail.com>
2022-11-28 14:00:57 +0100

    if

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,3 +9,7 @@ app = Flask(__name__)
 def hello_flask():
     """Retunr hello"""
     return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
